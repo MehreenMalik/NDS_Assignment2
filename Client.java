d2bd644d0ea244e26505bbc5af37979e80656277
Mehreen Malik <mehreenmalik95@gmail.com>
2016-05-04 16:23:28 +0100

    changes to the Client to implement JFrame

--- Client.java
+++ Client.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.rmi.*;
@@ -6,54 +8,74 @@ import java.rmi.*;
 import javax.swing.*;
 import javax.swing.text.AbstractDocument.Content;
 
-public class Client extends JFrame
+public class Client extends JFrame implements ActionListener
 {	
+	//global declarations as needed
+	JButton submitButton = new JButton("Submit");
+	JTextField fileNameTextFIeld = new JTextField(10);
+	static String filename;
+	
 	//constructor
 	Client()
 	{
-		super();
-		JLabel fileNameLabel = new JLabel("Enter the file location:");
-		Content pane = new JContentPane();
+		super("Assignment 2");
 		
-		Client.getContentPane().add(fileNameLabel, BorderLayout.CENTER);
-
+		Container contentPane = getContentPane();
+		JPanel panel = new JPanel();
+		
+		JLabel fileNameLabel = new JLabel("Enter the file location:");
+		fileNameTextFIeld.setText("/Users/Desktop/source.txt");
+		submitButton.addActionListener(this);
+		
+		panel.add(fileNameLabel, BorderLayout.CENTER);
+		panel.add(fileNameTextFIeld, BorderLayout.CENTER);
+		panel.add(submitButton, BorderLayout.CENTER);
+		contentPane.add(panel);
+		
 		setSize(300,300);
 		setVisible(true);
 	}
 	
+	public void actionPerformed(ActionEvent e) 
+	{
+		if(e.getSource()==submitButton)
+		{
+			String filename = fileNameTextFIeld.getText().toString();
+			try 
+			{
+				getRemoteFile();
+			} 
+			catch (MalformedURLException murle)
+			{
+				System.out.println();
+				System.out.println("MalformedURLException");
+				System.out.println(murle);
+			}
+			catch (RemoteException re)
+			{
+				System.out.println();
+				System.out.println("RemoteException");
+				System.out.println(re);
+			}
+			catch (NotBoundException nbe)
+			{
+				System.out.println();
+				System.out.println("NotBoundException");
+				System.out.println(nbe);
+			}
+			catch (java.lang.ArithmeticException ae)
+			{
+				System.out.println();
+				System.out.println("java.lang.ArithmeticException");
+				System.out.println(ae);
+			}
+		}
+	}
+	
 	public static void main(String[]args)
 	{
-		JFrame Client = new JFrame("Assignment 2");
-		Client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		try 
-		{
-			getRemoteFile();
-		} 
-		catch (MalformedURLException murle)
-		{
-			System.out.println();
-			System.out.println("MalformedURLException");
-			System.out.println(murle);
-		}
-		catch (RemoteException re)
-		{
-			System.out.println();
-			System.out.println("RemoteException");
-			System.out.println(re);
-		}
-		catch (NotBoundException nbe)
-		{
-			System.out.println();
-			System.out.println("NotBoundException");
-			System.out.println(nbe);
-		}
-		catch (java.lang.ArithmeticException ae)
-		{
-			System.out.println();
-			System.out.println("java.lang.ArithmeticException");
-			System.out.println(ae);
-		}
+		JFrame frame = new Client();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	//method that needs to open a file from the File Server node
 	public static void getRemoteFile() throws MalformedURLException, RemoteException, NotBoundException
@@ -63,7 +85,7 @@ public class Client extends JFrame
 		MyRemoteFileSystem myfile = (MyRemoteFileSystem)Naming.lookup(serverobjectname);
 		//proceed with remote invocation
 		//where filename is a string representing the name of the file.
-		String filename = new String("/Users/Desktop/source.txt");
+		//String filename = new String("/Users/Desktop/source.txt");
 		try 
 		{
 			int c = myfile.openFile(filename);
